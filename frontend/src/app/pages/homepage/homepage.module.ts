a236f88cdaa5e162c8cba632bc7bca3672c17317
ivanjimenezGM <ivan.jimenez@groupm.com>
2022-02-22 10:26:49 +0100

    Comienza el carousel de la home

--- frontend/src/app/pages/homepage/homepage.module.ts
+++ frontend/src/app/pages/homepage/homepage.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 
 @NgModule({
   declarations: [
-    HomepageComponent
+    HomepageComponent,
+    CarouselComponent
   ],
   imports: [
     CommonModule,
