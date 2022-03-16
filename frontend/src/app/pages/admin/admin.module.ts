07fbdc8fa31d9d90fd399518971a30434c06efb7
souljavan <jcavanilles@gmail.com>
2022-03-16 09:49:10 +0100

    cambio en tabla a ng prime2

--- frontend/src/app/pages/admin/admin.module.ts
+++ frontend/src/app/pages/admin/admin.module.ts
@@ -6,19 +6,22 @@ import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { SalasComponent } from './components/salas/salas.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PeliculasListComponent } from './components/peliculas-list/peliculas-list.component';
+import {TableModule} from 'primeng/table';
 
 
 @NgModule({
 	declarations: [
     PeliculasComponent,
     SalasComponent,
-    PeliculasListComponent
+    PeliculasListComponent,
+
   ],
 	imports: [
 		CommonModule,
 		AdminRoutingModule,
 		ReactiveFormsModule,
-		FormsModule
+		FormsModule,
+		TableModule
 	]
 })
 export class AdminModule { }
